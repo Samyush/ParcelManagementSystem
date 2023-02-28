33a97b52c9c3fd00a7a53f89b8201aedc9ff66e5
samyush <samyushmaharjan@gmail.com>
2023-03-01 00:07:07 +0545

    adding console application and api application

---
+++ CodeTesterConsoleApp/ClassOne.cs
@@ -0,0 +1,19 @@
+﻿using CodeTesterConsoleApp;
+
+namespace CodeTesterConsoleApp;
+
+public class ClassOne : ClassTwo
+{
+    
+}
+
+public abstract class ClassTwo
+{
+    PartialClass test = new();
+
+    public void tester()
+    {
+        PartialClass.tester();
+        Console.WriteLine("test");
+    }
+}
