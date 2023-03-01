967e178a7777d4fb4adda34afadacef270c2d8c5
Shaga01 <gautamshashwat1@gmail.com>
2023-03-01 13:12:35 +0545

    resolved issues

---
+++ ParcelManagementSystemMVC/Data/ParcelMgmtContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+public class ParcelMgmtContext : DbContext
+{
+	public ParcelMgmtContext(DbContextOptions<ParcelMgmtContext> options): base(options) { }
+
+    public DbSet<userList> ListofUsers{ get; set; }
+}
---
+++ ParcelManagementSystemMVC/Migrations/20230301010009_InitialDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelManagementSystemMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListofUsers",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Branch = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    External = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actions = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListofUsers");
+        }
+    }
+}
