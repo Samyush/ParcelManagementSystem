dbe78cf5c8abe1f892e08ce211fd6cba42c2318b
samyush <samyushmaharjan@gmail.com>
2023-03-20 13:49:35 +0545

    adding login page

---
+++ ParcelManagementSystemMVC/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParcelManagementSystemMVC.Controllers
+{
+    public class UserController : Controller
+    {
+
+        //LoginPage
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+        
+        //RegisterPage
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+        
+        //ForgotPasswordPage
+        public IActionResult ForgotPasswordPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ ParcelManagementSystemMVC/Views/User/RegisterPage.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParcelManagementSystemMVC.Views.User;
+
+public class RegisterPage : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
