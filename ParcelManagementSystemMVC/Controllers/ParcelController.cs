2b5c5936cf44e2ba005586b5c01a23bf6a547049
rujanshakya <rujan.shakya123@gmail.com>
2023-03-29 19:43:18 +0545

    'navbar

---
+++ ParcelManagementSystemMVC/Controllers/ParcelController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParcelManagementSystemMVC.Controllers
+{
+    public class ParcelController : Controller
+    {
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+        public IActionResult AddParcel()
+        {
+            return View();
+        }
+        public IActionResult ParcelList()
+        {
+            return View();
+        }
+        public IActionResult TrackParcel()
+        {
+            return View();
+        }
+    }
+}
