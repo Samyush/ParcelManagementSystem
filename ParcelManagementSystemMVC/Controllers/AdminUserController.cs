0e6a93ba6a507f57d04a24636eff9c3887106564
rujanshakya <rujan.shakya123@gmail.com>
2023-04-03 15:56:41 +0545

    'adminlayoutadded'

---
+++ ParcelManagementSystemMVC/Controllers/AdminUserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParcelManagementSystemMVC.Controllers
+{
+    public class AdminUserController : Controller
+    {
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
+
